0280c613d1ebe2eb7d5c2533faf728f9f865578a
Felipe Chagas <103911093+fpchagas@users.noreply.github.com>
2024-11-17 00:16:45 +0000

    1st commit

---
+++ pages/index.js
@@ -0,0 +1,4 @@
+function Home(){
+    return <h1>O Cristo não pediu muita coisa, não exigiu que as pessoas escalassem o Everest ou fizessem grandes sacrifícios. Ele só pediu que nos amássemos uns aos outros.</h1>
+}
+export default Home;
